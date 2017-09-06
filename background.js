782df0f6da675825643130dd5e87f6ddd61de3da
fiatjaf <fiatjaf@gmail.com>
2017-09-06 00:48:10 -0300

    print selection and print "article" version of the page.

--- background.js
+++ background.js
@@ -1,9 +1,14 @@
 /* global browser */
 
 browser.browserAction.onClicked.addListener(function () {
-  browser.tabs.executeScript({
-    file: '/vendor/gcp.js'
-  })
+  run('print-url')
+})
+
+function run (command) {
+  Promise.all([
+    browser.tabs.executeScript({file: '/vendor/gcp.js'}),
+    browser.tabs.executeScript({code: `window.cmd = "${command}"`})
+  ])
     .then(() =>
       browser.tabs.executeScript({
         file: '/script.js',
@@ -11,4 +16,30 @@ browser.browserAction.onClicked.addListener(function () {
       })
     )
     .catch(e => console.log('google cloud print loading error', e))
+}
+
+browser.contextMenus.create({
+  id: 'print-selection',
+  title: 'Print selection on Google Cloud Print',
+  contexts: ['selection']
+})
+
+browser.contextMenus.create({
+  id: 'print-page-as-article',
+  title: 'Print current page as article on Google Cloud Print',
+  contexts: ['page']
+})
+
+browser.contextMenus.onClicked.addListener(function (info, tab) {
+  switch (info.menuItemId) {
+    case 'print-raw-selection':
+      run('print-selection-raw')
+      break
+    case 'print-selection':
+      run('print-selection')
+      break
+    case 'print-page-as-article':
+      run('print-readable')
+      break
+  }
 })
--- script.js
+++ script.js
@@ -1,39 +1,40 @@
-/* global cloudprint */
+/* global fetch, cloudprint */
 
 var url = location.href
 var title = document.title
 
-// for (let i = 0; i < document.styleSheets.length; i++) {
-//   try {
-//     var sheet = document.styleSheets[i]
-//     var rules = sheet.cssRules
-//     console.log('looping through', sheet)
-//     for (let j = 0; j < rules.length; j++) {
-//       var rule = rules[j]
-//       if (rule.condition) {
-//         console.log(rule)
-//       }
-//     }
-//   } catch (e) {
-//     console.log('skipped', sheet)
-//   }
-// }
-
-var gadget = new cloudprint.Gadget()
-gadget.setPrintDocument('url', title, url)
-gadget.openPrintDialog()
-
-// function readable (url) {
-//   return fetch('https://mercury.postlight.com/parser?url=' + url, {
-//     headers: {
-//       'Content-Type': 'application/json',
-//       'Accept': 'application/json',
-//       'x-api-key': 'LKyckVL4j7x9LtByAUD4LfCQ40M0VdSoXW0VW6W9'
-//     }
-//   })
-//     .then(r => r.json())
-//     .then(data => ({
-//       title: data.title,
-//       content: data.content
-//     }))
-// }
+switch (window.cmd) {
+  case 'print-url':
+    let gadget = new cloudprint.Gadget()
+    gadget.setPrintDocument('url', title, url)
+    gadget.openPrintDialog()
+    break
+  case 'print-readable':
+    fetch('https://mercury.postlight.com/parser?url=' + url, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'x-api-key': 'LKyckVL4j7x9LtByAUD4LfCQ40M0VdSoXW0VW6W9'
+      }
+    })
+      .then(r => r.json())
+      .then(data => {
+        let gadget = new cloudprint.Gadget()
+        gadget.setPrintDocument('text/html', data.title, data.content)
+        gadget.openPrintDialog()
+      })
+    break
+  case 'print-selection':
+    var range
+    let selection = window.getSelection()
+    if (selection.rangeCount > 0) {
+      range = selection.getRangeAt(0)
+      let clonedSelection = range.cloneContents()
+      let div = document.createElement('div')
+      div.appendChild(clonedSelection)
+      let gadget = new cloudprint.Gadget()
+      gadget.setPrintDocument('text/html', title, div.innerHTML)
+      gadget.openPrintDialog()
+    }
+    break
+}
